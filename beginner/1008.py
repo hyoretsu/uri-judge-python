7565bc82390a843cf522f6bc101c7ac583feeb91
Aran Leite <hyoretsu@gmail.com>
2021-03-19 23:27:05 -0300

    Solved #1008

---
+++ beginner/1008.py
@@ -0,0 +1,6 @@
+employeeNumber = int(input())
+hoursWorked = int(input())
+salaryPerHour = float(input())
+
+print(f"NUMBER = {employeeNumber}")
+print(f"SALARY = U$ {salaryPerHour * hoursWorked:.2f}")
