5e543032a9e27968cf7c622419ad6f15634eab6c
Aran Leite <hyoretsu@gmail.com>
2021-03-20 13:36:27 -0300

    Solved #1013

---
+++ beginner/1013.py
@@ -0,0 +1,6 @@
+# greatestNumber= max(map(int, input().rsplit(" ")))
+[a, b, c] = map(int, input().rsplit(" "))
+abGreatest = (a + b + abs(a - b)) / 2
+greatestNumber = (abGreatest + c + abs(abGreatest - c)) / 2
+
+print(f"{greatestNumber:.0f} eh o maior")
