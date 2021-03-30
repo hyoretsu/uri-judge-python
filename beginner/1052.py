120a72f621004b24b9479df55c5dbaba99ce5653
Aran Leite <hyoretsu@gmail.com>
2021-03-30 17:20:18 -0300

    Solved #1052

---
+++ beginner/1052.py
@@ -0,0 +1,19 @@
+monthNumber = int(input())
+
+months = [
+    "",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+print(f"{months[monthNumber]}")
